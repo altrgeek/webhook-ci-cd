c56e33081c617d96a4af608913e4256ae4352e87
Rumman Zaman <altrgeek@gmail.com>
2022-12-24 02:55:09 +0600

    Update deploy.sh

--- webhooks/name_of_the_github_repo/deploy.sh
+++ webhooks/name_of_the_github_repo/deploy.sh
@@ -2,4 +2,8 @@
 
 git fetch --all
 git checkout --force "origin/main"
+npm install --legacy-peer-deps --prefix /root/github_repo/api/
+npm i && rm -r build/ && npm run build
+systemctl reload nginx
+systemctl restart backend
 
